2cfc84f7afa912b27c979413bdec636f8469810c
brandonlund <brandonlund2019@gmail.com>
2020-04-17 13:04:20 -0500

    Add files via upload

--- tictactoe/tictactoe.cpp
+++ tictactoe/tictactoe.cpp
@@ -1,171 +1,176 @@
-// tictactoe.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 
 using namespace std;
 
-void make_move(char board[3][3], bool turn, char &player);
-void draw_board(char board[3][3]);
-void check_win(char board[3][3], int &winner);
+void make_move( char board[ 3 ][ 3 ], bool turn, char &player );
+void draw_board( char board[ 3 ][ 3 ] );
+void check_win( char board[ 3 ][ 3 ], int &winner );
+void clear_board( char board[ 3 ][ 3 ] );
 
 int main()
 {
-	char board[3][3] = { 0 
-					   /*{ 'X', 'X', 'X' },
-						 { 'X', 'X', 'X' }, 
-						 { 'X', 'X', 'X' }*/ };
-	char player = 0;
-	bool turn = 1;
-	int winner = 0; // 0 no winner, 1 winner, 2 tie
+    char board[ 3 ][ 3 ] = { 0 };
+    char player = 0;
+    bool turn = 1;
+    int winner = 0;
+    char replay = 'y';
 
-	draw_board(board);
+    while ( replay == 'y' )
+    {
+        winner = 0;
+        turn = 1;
+        player = 0;
+        clear_board( board );
+        draw_board( board );
 
-	while (!winner)
-	{
-		turn = !turn;
-		make_move(board, turn, player);
-		draw_board(board);
-		check_win(board, winner);
-	}
+        while ( !winner )
+        {
+            turn = !turn;
+            make_move( board, turn, player );
+            draw_board( board );
+            check_win( board, winner );
+        }
 
-	if ( winner == 1 )
-	{
-		cout << "Congrats, Player " << player << "! You've won!!" << endl;
-	}
-	else
-	{
-		cout << "Better luck next time! It was a tie!" << endl;
-	}
+        if ( winner == 1 )
+        {
+            cout << "Congrats, Player " << player << "! You've won!!" << endl;
+            cout << "Would you like to play again? y/n: ";
+            cin >> replay;
+        }
+        else
+        {
+            cout << "Better luck next time! It was a tie!" << endl;
+            cout << "Would you like to play again? y/n: ";
+            cin >> replay;
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
-void make_move(char board[3][3], bool turn, char &player)
+void make_move( char board[ 3 ][ 3 ], bool turn, char &player )
 {
-	int row, col;
+    int row, col;
 
-	if (!turn)
-	{
-		player = 'X';
-	}
-	else
-	{
-		player = 'O';
-	}
+    if ( !turn )
+    {
+        player = 'X';
+    }
+    else
+    {
+        player = 'O';
+    }
 
-	cout << "Player " << player << ", Please enter your row: ";
-	cin >> row;
-	cout << "Player " << player << ", Please enter your column: ";
-	cin >> col;
+    cout << "Player " << player << ", Please enter your row: ";
+    cin >> row;
+    cout << "Player " << player << ", Please enter your column: ";
+    cin >> col;
 
-	row -= 1;
-	col -= 1;
+    row -= 1;
+    col -= 1;
 
-	while (board[row][col] == 'X' || board[row][col] == 'O')
-	{
-		cout << "Coordinate taken. Please choose new coordinates." << endl;
+    while ( board[ row ][ col ] == 'X' || board[ row ][ col ] == 'O' )
+    {
+        cout << "Coordinate taken. Please choose new coordinates." << endl;
 
-		cout << "Player " << player << ", Please enter your row: ";
-		cin >> row;
-		cout << "Player " << player << ", Please enter your column: ";
-		cin >> col;
+        cout << "Player " << player << ", Please enter your row: ";
+        cin >> row;
+        cout << "Player " << player << ", Please enter your column: ";
+        cin >> col;
 
-		row -= 1;
-		col -= 1;
-	}
+        row -= 1;
+        col -= 1;
+    }
 
-	board[row][col] = player;
+    board[ row ][ col ] = player;
 }
 
-void draw_board(char board[3][3])
+void draw_board( char board[ 3 ][ 3 ] )
 {
-	int r, c;
+    int r, c;
 
-	system("cls");
+    system( "cls" );
 
-	cout << endl;
-	cout << "    1   2   3" << endl;
+    cout << endl;
+    cout << "    1   2   3" << endl;
 
-	for (r = 0; r < 3; r++)
-	{
-		cout << " ";
-		for (c = 0; c < 3; c++)
-		{
-			if (c == 0)
-			{
-				cout << " " << r + 1 << " ";
-			}
+    for ( r = 0; r < 3; r++ )
+    {
+        cout << " ";
+        for ( c = 0; c < 3; c++ )
+        {
+            if ( c == 0 )
+            {
+                cout << " " << r + 1 << " ";
+            }
 
-			cout << board[r][c];
+            cout << board[ r ][ c ];
 
-			if ( c < 2)
-			{
-				cout << " | ";
-			}
-		}
-		if (r < 2)
-		{
-			cout << endl << " " << "  -----------" << endl;
-		}
-	}
+            if ( c < 2 )
+            {
+                cout << " | ";
+            }
+        }
+        if ( r < 2 )
+        {
+            cout << endl << " " << "  -----------" << endl;
+        }
+    }
 
-	cout << endl << endl;
+    cout << endl << endl;
 
 }
-void check_win(char board[3][3], int & winner)
+void check_win( char board[ 3 ][ 3 ], int & winner )
 {
-	if (   // ROWS X
-		   (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X')
-		|| (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X')
-		|| (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X')
-		   // COLS X
-		|| (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X')
-		|| (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X')
-		|| (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X')
-		   // DIAGS X
-		|| (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X')
-		|| (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X')
-		   // ROWS O
-		|| (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O')
-		|| (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O')
-		|| (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O')
-		   // COLS O
-		|| (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O')
-		|| (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O')
-		|| (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')
-		   // DIAGS O
-		|| (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O')
-		|| (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O')
-		)
-	{
-		winner = 1;
-	}
-	else if ((board[0][0] == 'X' || board[0][0] == 'O')
-			&& (board[0][1] == 'X' || board[0][1] == 'O')
-			&& (board[0][2] == 'X' || board[0][2] == 'O')
-			&& (board[1][0] == 'X' || board[1][0] == 'O')
-			&& (board[1][1] == 'X' || board[1][1] == 'O')
-			&& (board[1][2] == 'X' || board[1][2] == 'O')
-			&& (board[2][0] == 'X' || board[2][0] == 'O')
-			&& (board[2][1] == 'X' || board[2][1] == 'O')
-			&& (board[2][2] == 'X' || board[2][2] == 'O')
-		)
-	{
-		winner = 2;
-	}
-	else
-	{
-		winner = 0;
-	}
+    if (    ( board[ 0 ][ 0 ] == 'X' && board[ 0 ][ 1 ] == 'X' && board[ 0 ][ 2 ] == 'X' )
+         || ( board[ 1 ][ 0 ] == 'X' && board[ 1 ][ 1 ] == 'X' && board[ 1 ][ 2 ] == 'X' )
+         || ( board[ 2 ][ 0 ] == 'X' && board[ 2 ][ 1 ] == 'X' && board[ 2 ][ 2 ] == 'X' )
+         || ( board[ 0 ][ 0 ] == 'X' && board[ 1 ][ 0 ] == 'X' && board[ 2 ][ 0 ] == 'X' )
+         || ( board[ 0 ][ 1 ] == 'X' && board[ 1 ][ 1 ] == 'X' && board[ 2 ][ 1 ] == 'X' )
+         || ( board[ 0 ][ 2 ] == 'X' && board[ 1 ][ 2 ] == 'X' && board[ 2 ][ 2 ] == 'X' )
+         || ( board[ 0 ][ 0 ] == 'X' && board[ 1 ][ 1 ] == 'X' && board[ 2 ][ 2 ] == 'X' )
+         || ( board[ 0 ][ 2 ] == 'X' && board[ 1 ][ 1 ] == 'X' && board[ 2 ][ 0 ] == 'X' )
+         || ( board[ 0 ][ 0 ] == 'O' && board[ 0 ][ 1 ] == 'O' && board[ 0 ][ 2 ] == 'O' )
+         || ( board[ 1 ][ 0 ] == 'O' && board[ 1 ][ 1 ] == 'O' && board[ 1 ][ 2 ] == 'O' )
+         || ( board[ 2 ][ 0 ] == 'O' && board[ 2 ][ 1 ] == 'O' && board[ 2 ][ 2 ] == 'O' )
+         || ( board[ 0 ][ 0 ] == 'O' && board[ 1 ][ 0 ] == 'O' && board[ 2 ][ 0 ] == 'O' )
+         || ( board[ 0 ][ 1 ] == 'O' && board[ 1 ][ 1 ] == 'O' && board[ 2 ][ 1 ] == 'O' )
+         || ( board[ 0 ][ 2 ] == 'O' && board[ 1 ][ 2 ] == 'O' && board[ 2 ][ 2 ] == 'O' )
+         || ( board[ 0 ][ 0 ] == 'O' && board[ 1 ][ 1 ] == 'O' && board[ 2 ][ 2 ] == 'O' )
+         || ( board[ 0 ][ 2 ] == 'O' && board[ 1 ][ 1 ] == 'O' && board[ 2 ][ 0 ] == 'O' )
+         )
+    {
+        winner = 1;
+    }
+    else if (    ( board[ 0 ][ 0 ] == 'X' || board[ 0 ][ 0 ] == 'O' )
+              && ( board[ 0 ][ 1 ] == 'X' || board[ 0 ][ 1 ] == 'O' )
+              && ( board[ 0 ][ 2 ] == 'X' || board[ 0 ][ 2 ] == 'O' )
+              && ( board[ 1 ][ 0 ] == 'X' || board[ 1 ][ 0 ] == 'O' )
+              && ( board[ 1 ][ 1 ] == 'X' || board[ 1 ][ 1 ] == 'O' )
+              && ( board[ 1 ][ 2 ] == 'X' || board[ 1 ][ 2 ] == 'O' )
+              && ( board[ 2 ][ 0 ] == 'X' || board[ 2 ][ 0 ] == 'O' )
+              && ( board[ 2 ][ 1 ] == 'X' || board[ 2 ][ 1 ] == 'O' )
+              && ( board[ 2 ][ 2 ] == 'X' || board[ 2 ][ 2 ] == 'O' )
+              )
+    {
+        winner = 2;
+    }
+    else
+    {
+        winner = 0;
+    }
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void clear_board( char board[ 3 ][ 3 ] )
+{
+    int r, c;
+
+    for ( r = 0; r < 3; r++ )
+    {
+        for ( c = 0; c < 3; c++ )
+        {
+            board[ r ][ c ] = 0;
+        }
+    }
+}
